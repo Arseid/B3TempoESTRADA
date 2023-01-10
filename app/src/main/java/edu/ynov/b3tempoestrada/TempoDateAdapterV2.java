d9c72a885011e5cb3c172ad03ab0d64d3167999e
Arseid <yvesrobert.estrada@gmail.com>
2023-01-10 11:56:08 +0100

    Evolution 3 done

--- app/src/main/java/edu/ynov/b3tempoestrada/TempoDateAdapterV2.java
+++ app/src/main/java/edu/ynov/b3tempoestrada/TempoDateAdapterV2.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 import edu.ynov.b3tempoestrada.databinding.TempoDateItemBinding;
+import edu.ynov.b3tempoestrada.databinding.TempoDateItemV2Binding;
 
 public class TempoDateAdapterV2 extends RecyclerView.Adapter<TempoDateAdapterV2.TempoDateViewHolderV2>{
 
@@ -26,14 +27,15 @@ public class TempoDateAdapterV2 extends RecyclerView.Adapter<TempoDateAdapterV2.
     @NonNull
     @Override
     public TempoDateAdapterV2.TempoDateViewHolderV2 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.tempo_date_item, parent,false);
-        TempoDateItemBinding binding = TempoDateItemBinding.bind(v);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.tempo_date_item_v2, parent,false);
+        TempoDateItemV2Binding binding = TempoDateItemV2Binding.bind(v);
         return new TempoDateAdapterV2.TempoDateViewHolderV2(binding);
     }
 
     @Override
     public void onBindViewHolder(@NonNull TempoDateAdapterV2.TempoDateViewHolderV2 holder, int position) {
         holder.binding.dateTv.setText(tempoDates.get(position).getDate());
+        holder.binding.colorTv.setText(tempoDates.get(position).getCouleur().toString());
         holder.binding.colorFl.setBackgroundColor(ContextCompat.getColor(context, tempoDates.get(position).getCouleur().getColorResId()));
     }
 
@@ -41,9 +43,9 @@ public class TempoDateAdapterV2 extends RecyclerView.Adapter<TempoDateAdapterV2.
     public int getItemCount() { return tempoDates.size(); }
 
     public class TempoDateViewHolderV2 extends RecyclerView.ViewHolder {
-        TempoDateItemBinding binding;
+        TempoDateItemV2Binding binding;
 
-        public TempoDateViewHolderV2(@NonNull TempoDateItemBinding binding) {
+        public TempoDateViewHolderV2(@NonNull TempoDateItemV2Binding binding) {
             super(binding.getRoot());
             this.binding = binding;
         }
